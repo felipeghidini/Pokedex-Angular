ca62725aeb6804f9e17201ac308e09c1e7e1f7ec
felipeghidini <lipeghidini18@gmail.com>
2021-09-27 22:31:25 -0300

    Criado componente card e lista de pokemons

---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.sass']
+})
+export class PokemonListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  pokemons = ['Bulbasaur', 'Ivysaur'];
+
+}
